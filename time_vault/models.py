758cffe4b2414ba5e7e425a722d760bb06870986
Christian Grossmüller <chgad.games@gmail.com>
2023-11-17 17:10:53 +0100

    Add check for shift_content Dict keys to be of format DD.MM.YYYY .

--- time_vault/models.py
+++ time_vault/models.py
@@ -1,5 +1,6 @@
 from typing import Dict
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
+from datetime import datetime
 
 
 class Day(BaseModel):
@@ -14,3 +15,14 @@ class Report(BaseModel):
     shift_content: Dict[str, Day]
     general: GeneralInfo
 
+    @field_validator("general")
+    @classmethod
+    def check_date_string(cls, dictionary: Dict) -> Dict:
+        """
+        Check if all keys in a Dict are of DD.MM.YYYY format.
+        :param dictionary:
+        :return:
+        """
+        for key in dictionary.keys():
+            datetime.strptime(key, "%d.%m.%Y")
+        return dictionary
