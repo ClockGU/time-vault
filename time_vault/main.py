e9de7426a0b38445148b71151ddcec6021080e51
Christian Grossmüller <chgad.games@gmail.com>
2024-05-23 18:49:55 +0200

    Correctly format.

--- time_vault/main.py
+++ time_vault/main.py
@@ -1,10 +1,12 @@
+from typing import Annotated
+
 import sentry_sdk
-from fastapi import FastAPI, Depends
+from fastapi import Depends, FastAPI, Query
 from fastapi.security.api_key import APIKey
 
-from .database import save_report_document
-from .models import Report
 from .auth import required_api_key
+from .database import get_report_collection, save_report_document
+from .models import Report
 from .settings import get_settings
 
 SETTINGS = get_settings()
@@ -17,3 +19,13 @@ app = FastAPI()
 @app.post("/reports/", status_code=201)
 def save_report(report: Report, api_key: APIKey = Depends(required_api_key)):
     return save_report_document(report)
+
+
+@app.get("/retrieve/{month}/{year}", status_code=200)
+def get_reports(references: Annotated[list, Query()], month: int, year: int):
+    collection = get_report_collection()
+    result = collection.find(
+        {"month": month, "year": year, "general.reference": {"$in": references}}
+    )
+
+    return map(Report, result)
