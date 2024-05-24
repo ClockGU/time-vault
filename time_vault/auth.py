23e5e67cec3e784abd56cc8650b1e9dfc9bd1f48
Christian Grossmüller <chgad.games@gmail.com>
2024-05-24 17:32:08 +0200

    Reformat.

--- time_vault/auth.py
+++ time_vault/auth.py
@@ -1,5 +1,5 @@
+from fastapi import Depends, HTTPException, Security
 from fastapi.security.api_key import APIKeyHeader
-from fastapi import Security, HTTPException, Depends
 from starlette.status import HTTP_403_FORBIDDEN
 
 from .settings import Settings, get_settings
@@ -7,7 +7,11 @@ from .settings import Settings, get_settings
 api_key_header = APIKeyHeader(name="X-API-KEY", auto_error=False)
 
 
-def required_api_key(settings: Settings = Depends(get_settings), api_key: str = Security(api_key_header)):
+def required_api_key(
+    settings: Settings = Depends(get_settings), api_key: str = Security(api_key_header)
+):
     if api_key == settings.API_KEY:
         return api_key_header
-    raise HTTPException(status_code=HTTP_403_FORBIDDEN, detail="No valid API KEY provided.")
+    raise HTTPException(
+        status_code=HTTP_403_FORBIDDEN, detail="No valid API KEY provided."
+    )
