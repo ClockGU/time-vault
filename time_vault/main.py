fa08448239bf25c8f9042e8348f478226f602ee0
Christian Grossmüller <chgad.games@gmail.com>
2024-08-06 18:20:29 +0200

    move setup logger to differnt place.

--- time_vault/main.py
+++ time_vault/main.py
@@ -1,4 +1,8 @@
+import logging
+import os
 from contextlib import asynccontextmanager
+from datetime import datetime
+from logging.handlers import TimedRotatingFileHandler
 from typing import Annotated
 
 import sentry_sdk
@@ -9,7 +13,6 @@ from fastapi.security.api_key import APIKey
 
 from .auth import required_api_key
 from .database import get_report_collection, save_report_document
-from .log import setup_logger
 from .models import Report
 from .settings import get_settings
 from .tasks import deprovision_reports
@@ -17,9 +20,6 @@ from .tasks import deprovision_reports
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # Setup Logger
-    setup_logger()
-    # Setup Scheduler
     scheduler = BackgroundScheduler()
     scheduler.add_job(deprovision_reports, trigger=CronTrigger(day=1))
     scheduler.start()
@@ -32,6 +32,36 @@ if SETTINGS.DEBUG is False:
     sentry_sdk.init(dsn=SETTINGS.SENTRY_URL, enable_tracing=True)
 
 app = FastAPI(debug=SETTINGS.DEBUG, lifespan=lifespan)
+
+# Get the absolute path of the current file
+current_file_path = os.path.abspath(__file__)
+# Get the directory containing the current file
+current_directory = os.path.dirname(current_file_path)
+# Go up one directory level
+ROOT_DIR = os.path.dirname(current_directory)
+
+LOG_ROOT = os.path.join(ROOT_DIR, "logs")
+
+try:
+    os.mkdir(LOG_ROOT)
+except FileExistsError:
+    pass
+logger = logging.getLogger("deprovisioning")
+logger.setLevel(logging.DEBUG)
+ch = TimedRotatingFileHandler(
+    os.path.join(str(LOG_ROOT), "deprovision.log"),
+    when="D",
+    interval=1,
+    backupCount=31,
+)
+
+logger.addHandler(ch)
+logger.debug(f"INFO: Logger setup at {datetime.now().strftime('%d.%m.%Y - %H:%M:%S')}")
+
+
+@app.on_event("startup")
+async def startup_event():
+    logger.info("FastAPI app is starting up")
 
 
 @app.post("/reports/", status_code=201)
