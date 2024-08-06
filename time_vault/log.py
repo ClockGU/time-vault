51ad32aaab1866e3cbb88d96f69878f89dbc5eed
Christian Grossmüller <chgad.games@gmail.com>
2024-08-06 16:32:52 +0200

    Move directory determination into function.

--- time_vault/log.py
+++ time_vault/log.py
@@ -2,17 +2,17 @@ import logging
 import os
 from logging.handlers import TimedRotatingFileHandler
 
-# Get the absolute path of the current file
-current_file_path = os.path.abspath(__file__)
-# Get the directory containing the current file
-current_directory = os.path.dirname(current_file_path)
-# Go up one directory level
-ROOT_DIR = os.path.dirname(current_directory)
-
-LOG_ROOT = os.path.join(ROOT_DIR, "logs")
-
 
 def setup_logger():
+    # Get the absolute path of the current file
+    current_file_path = os.path.abspath(__file__)
+    # Get the directory containing the current file
+    current_directory = os.path.dirname(current_file_path)
+    # Go up one directory level
+    ROOT_DIR = os.path.dirname(current_directory)
+
+    LOG_ROOT = os.path.join(ROOT_DIR, "logs")
+
     try:
         os.mkdir(LOG_ROOT)
     except FileExistsError:
