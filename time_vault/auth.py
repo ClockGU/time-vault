edee4475185085c446600e6ad1baecddc1beb93b
Christian Grossmüller <chgad.games@gmail.com>
2026-05-11 17:52:13 +0200

    Add endpoint to delete reports extra secured by hased api key with keyword.

--- time_vault/auth.py
+++ time_vault/auth.py
@@ -1,3 +1,5 @@
+from hashlib import sha256
+
 from fastapi import Depends, HTTPException, Security
 from fastapi.security.api_key import APIKeyHeader
 from starlette.status import HTTP_403_FORBIDDEN
@@ -11,6 +13,18 @@ def required_api_key(
     settings: Settings = Depends(get_settings), api_key: str = Security(api_key_header)
 ):
     if api_key == settings.API_KEY:
+        return api_key_header
+    raise HTTPException(
+        status_code=HTTP_403_FORBIDDEN, detail="No valid API KEY provided."
+    )
+
+
+def required_hashed_api_key(
+    settings: Settings = Depends(get_settings), api_key: str = Security(api_key_header)
+):
+    hash = bytes(settings.ADMIN_KEYWORD, "utf-8") + bytes(settings.API_KEY, "utf-8")
+    hashed_key = sha256(hash).hexdigest()
+    if api_key == hashed_key:
         return api_key_header
     raise HTTPException(
         status_code=HTTP_403_FORBIDDEN, detail="No valid API KEY provided."
